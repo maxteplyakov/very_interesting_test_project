65c6c4a291ea496b1246d134993853792fbb51c1
Max Teplyakov <maxteplyakov@gmail.com>
2020-12-27 17:07:56 +0700

    Create Memory model and register it in admin panel

--- very_interesting_test_project/memories/admin.py
+++ very_interesting_test_project/memories/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Memory
+
+
+class MemoriesAdmin(admin.ModelAdmin):
+    list_display = ('place_name', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Memory, MemoriesAdmin)
--- very_interesting_test_project/memories/models.py
+++ very_interesting_test_project/memories/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Memory(models.Model):
+    place_name = models.CharField(max_length=200, verbose_name='Название места')
+    text = models.TextField(verbose_name='Воспоминание')
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="memories")
+
+    # address =
+    # geolocation =
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.place_name
