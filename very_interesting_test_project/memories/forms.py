e0d9be386f8aa2eaa3bdec7ac8ab67646b03a584
Max Teplyakov <maxteplyakov@gmail.com>
2020-12-28 13:05:56 +0700

    Add styles for new_memory_form

--- very_interesting_test_project/memories/forms.py
+++ very_interesting_test_project/memories/forms.py
@@ -1,12 +1,13 @@
-from .models import Memory
 from django import forms
+
+from .models import Memory
 from .widgets import GoogleMapsAddressWidget
 
 
 class MemoryForm(forms.ModelForm):
     class Meta(object):
         model = Memory
-        fields = ['address', 'geolocation', 'place_name', 'text']
+        fields = ['place_name', 'text', 'address', 'geolocation']
         widgets = {
-            "address": GoogleMapsAddressWidget,
+            'address': GoogleMapsAddressWidget,
         }
