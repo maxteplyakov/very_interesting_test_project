fe323bc636e3b0f1c3cc44d05400924a7e0be8e3
Max Teplyakov <maxteplyakov@gmail.com>
2020-12-28 15:22:57 +0700

    Add 404/500 handlers

--- very_interesting_test_project/memories/views.py
+++ very_interesting_test_project/memories/views.py
@@ -45,3 +45,20 @@ def new_memory(request):
         form.save()
         return redirect('/')
     return render(request, 'new_memory_form.html', {'form': form})
+
+
+def page_not_found(request, exception):
+    return render(
+        request,
+        "misc/404.html",
+        {"path": request.path},
+        status=404
+    )
+
+
+def server_error(request):
+    return render(
+        request,
+        "misc/500.html",
+        status=500
+    )
--- very_interesting_test_project/very_interesting_test_project/urls.py
+++ very_interesting_test_project/very_interesting_test_project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+handler404 = "memories.views.page_not_found"
+handler500 = "memories.views.server_error"
+
 urlpatterns = [
     path('', include('memories.urls')),
     path('admin/', admin.site.urls),
